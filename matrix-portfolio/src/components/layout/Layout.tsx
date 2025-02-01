acf26ebe6eb16bb9da82a6cc77b988c39d1ab589
mattwebbyy <mattwebb022@gmail.com>
2025-02-01 22:17:11 +1100

    fix

--- matrix-portfolio/src/components/layout/Layout.tsx
+++ matrix-portfolio/src/components/layout/Layout.tsx
@@ -7,7 +7,7 @@ interface LayoutProps {
 export const Layout = ({ children }: LayoutProps) => {
   return (
     <div className="relative min-h-screen">
-      <div className="content relative z-10 p-8 text-center flex-grow flex flex-col justify-center items-center bg-black/70 opacity-0 fade-in">
+      <div className="content relative z-10 p-8 text-center flex-grow flex flex-col justify-center items-center  opacity-0 fade-in">
         {children}
       </div>
     </div>
