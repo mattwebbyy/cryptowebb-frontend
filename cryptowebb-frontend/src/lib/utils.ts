aeba460f95b178339b693aca9511df80953f870f
mattwebbyy <mattwebb022@gmail.com>
2025-02-02 20:16:26 +1100

    test

--- cryptowebb-frontend/src/lib/utils.ts
+++
@@ -1,7 +0,0 @@
-export const generateRandomChar = (chars: string) => {
-    return chars[Math.floor(Math.random() * chars.length)];
-  };
-  
-  export const randomRange = (min: number, max: number) => {
-    return Math.random() * (max - min) + min;
-  };
